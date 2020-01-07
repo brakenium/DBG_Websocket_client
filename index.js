30246d0ae567e259f0b9a58f992132a0e19db013
Brakenium <bramvk12@gmail.com>
2020-01-07 21:53:09 +0100

    Minor changes
    Added comments, moved some stuff around and removed unnecessary commented code

--- index.js
+++ index.js
@@ -10,24 +10,34 @@ const constants = require('./json/constants.json');
 // Defines all websockets
 const DBGWebsocket = new WebSocket(`wss://push.planetside2.com/streaming?environment=ps2&service-id=s:${config.dbg_api.service_id}`);
 const internalWSServer = new WebSocket.Server({ port: 8080 });
-const internalWS = new WebSocket('ws://0.0.0.0:8080');
 
-// This function saves files
-function saveData(dir, data) {
-	fs.writeFile(dir, data, function(err) {
+// This function saves files to a specified filepath and throws an error if it has one
+function saveData(filepath, data) {
+	fs.writeFile(filepath, data, function(err) {
 		if (err) throw err;
-		// console.log('Saved!');
 	});
 }
 
-// This function reads data from a file and sends it to the websocket which is the first variable (ws)
+// This function reads data from a file and sends it to the websocket which is the first variable (ws).
+// To do this it uses the properties and zones variables which gives it everything it needs to find the files which need to be send.
 function resendMetagameEvent(properties, zones, ws) {
+	// Loops the code for every zone
 	for (var i = 0, len = zones.length; i < len; i++) {
-		const dir = `./json/${properties[0]}/${properties[1]}/${properties[2]}/zone_${zones[i]}.json`;
-		if (fs.existsSync(dir)) {
-			const data = JSON.stringify(require(dir));
+
+		const filepath = `./json/${properties[0]}/${properties[1]}/${properties[2]}/zone_${zones[i]}.json`;
+
+		// Checks if the filepath exists
+		if (fs.existsSync(filepath)) {
+
+			// Saves the content in a variable
+			const data = JSON.stringify(require(filepath));
+
+			// Sends the content to the websocket specified in the 'ws' variable
 			console.log(`(IWS) Sending saved data to the websocket for zone: ${zones[i]}`);
 			ws.send(data);
+
+		} else {
+			console.log(`The filepath for zone: ${zones[i]}, doesn't exist, not send`);
 		}
 	}
 };
@@ -43,9 +53,11 @@ internalWSServer.on('connection', function connection(ws) {
 
 		// This switch statement holds all internal websocket commands
 		switch(message) {
+
 		// This command grabs the last MetagameEvents (alerts) from Miller that are in the json folder
 		case 'send_last_MetagameEvents_Miller': {
-			// This defines the properties needed for the MetagameEvent directory
+
+			// This defines the properties needed for the MetagameEvent filepath
 			const properties = ['MetagameEvent', 'Miller', 'last'];
 			const zones = ['2', '4', '6', '8', 'other'];
 
@@ -62,38 +74,58 @@ internalWSServer.on('connection', function connection(ws) {
 	// 		2.Nothing yet....
 	DBGWebsocket.on('open', function open() {
 		console.log('DBG websocket open');
+
+		// Listens to MetagameEvents on the DBGWebsocket on world 10 (Miller)
 		DBGWebsocket.send('{"service":"event","action":"subscribe","worlds":["10"],"eventNames":["MetagameEvent"]}');
 
 		// This let's client "DBGWebsocket" listen to incoming messages and puts the data that needs to be saved in the right location
 		DBGWebsocket.on('message', function incoming(data) {
-			const parsedData = JSON.parse(data);
-			let dir;
 
+			// Parses 'data' and stores it in 'parsedData'
+			const parsedData = JSON.parse(data);
+			let filepath;
+
+			// Filters the parsedData.types
 			switch(parsedData.type) {
-			default: {
-				console.log(`(ns) Type is not specified in filter: ${parsedData.type}`);
+			case 'serviceMessage': {
+				console.log(`(sM) Type is "${parsedData.type}"`);
+				
+				// After receiving the serviceMessage this will immediately forward it to the internal websocket to minimize delays
+				ws.send(data);
+
+				// Grabs the worldname from 'constants.json'. It grabs it from the worlds map and filters it to only show entries with the world ID received from the DBG API
+				// It then grabs the name from entry and saves it in world_name
+				const world_name = JSONPath(`$.worlds[?(@.world_id==${parsedData.payload.world_id})].name`, constants);
+
+				// Grabs the zone ID from 'constants.json'. It grabs it from metagame_event_list map and filters it to only show entries with the metagame_event_id from the DBG API
+				// Then it grabs the zone_id from the entry and saves it in zone_id
+				const zone_id = JSONPath(`$.metagame_event_list[?(@.metagame_event_id==${parsedData.payload.metagame_event_id})].zone_id`, constants);
+
+				// Logs the above defined variables
+				console.log(world_name);
+				console.log(zone_id);
+
+				// Defines the filepath and saves the data in that file
+				filepath = `./json/${parsedData.payload.event_name}/${world_name}/last/zone_${zone_id}.json`;
+				saveData(filepath, data);
+
+				// Checks if zone_id is other
+				if (zone_id == 'other') {
+					// Defines the necessary variables to save the alert with zone_id: 'other' in a file and saves it there
+					const date = new Date();
+					const filepath_all_other = `./json/${parsedData.payload.event_name}/${world_name}/all_other/${date}.json`;
+					saveData(filepath_all_other, data);
+				}
 				break;
 			}
 			case 'heartbeat': {
-				// console.log(`(hb) Type is "${parsedData.type}"`);
-				dir = './json/heartbeat/last/heartbeat.json';
-				saveData(dir, data);
+				// Saves the received heartbeat to a file. Only the last heartbeat will be in that file
+				filepath = './json/heartbeat/last/heartbeat.json';
+				saveData(filepath, data);
 				break;
 			}
-			case 'serviceMessage': {
-				console.log(`(sM) Type is "${parsedData.type}"`);
-				ws.send(data);
-				const world_name = JSONPath(`$.worlds[?(@.world_id==${parsedData.payload.world_id})].name`, constants);
-				const zone_id = JSONPath(`$.metagame_event_list[?(@.metagame_event_id==${parsedData.payload.metagame_event_id})].zone_id`, constants);
-				console.log(world_name);
-				console.log(zone_id);
-				dir = `./json/${parsedData.payload.event_name}/${world_name}/last/zone_${zone_id}.json`;
-				saveData(dir, data);
-				if (zone_id == 'other') {
-					const date = new Date();
-					dir = `./json/${parsedData.payload.event_name}/${world_name}/all_other/${date}.json`;
-					saveData(dir, data);
-				}
+			default: {
+				console.log(`(ns) Type is not specified in filter: ${parsedData.type}`);
 				break;
 			}
 			}
